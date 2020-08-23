eb68e3440ef72860e71b83eb7c877a55f04e0127
moscalir <moscalir@uni.coventry.ac.uk>
2020-08-23 13:47:19 +0300

    initiate portfolio

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+
+const Footer = () =>(
+
+    <div>
+        <h1>Welcome to Index page </h1>
+    </div>
+)
+
+export default Footer;
---
+++ components/Layout.js
@@ -0,0 +1,9 @@
+
+const Layout = () =>(
+
+    <div>
+        <h1>Welcome to Index page </h1>
+    </div>
+)
+
+export default Layout;
