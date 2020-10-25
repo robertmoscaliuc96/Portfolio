cc86dadeb118e7c28648615d821fe767a97c11be
moscalir <moscalir@uni.coventry.ac.uk>
2020-10-25 16:41:52 +0000

    Add Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,27 @@
 import { config, library } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as gtag from '../lib/gtag'
 
 
 import '../style/index.css'
 config.autoAddCss = false;
 
-export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
-  }
+const App = ({ Component, pageProps }) => {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
+  return <Component {...pageProps} />
+}
+
+export default App
