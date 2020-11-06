7d526db50746033eafe481a4c059165fdde4e49b
moscalir <moscalir@uni.coventry.ac.uk>
2020-11-06 18:25:45 +0000

    Contact Form Google maps

--- components/layout/Map.js
+++ components/layout/Map.js
@@ -1,10 +1,11 @@
 import { Component } from 'react';
 import ReactMapGL, {Marker}  from 'react-map-gl';
 
+
 class Map extends Component {
   state = {
     viewport: {
-      width: '55vw',
+      width: '100vh',
       height: '100vh',
       latitude: 53.475433,
       longitude: -2.236497,
@@ -16,35 +17,34 @@ class Map extends Component {
 
   render() {
     return (
-      <ReactMapGL
-        mapStyle="mapbox://styles/mapbox/dark-v10"
-        mapboxApiAccessToken="pk.eyJ1IjoibW9zY2FsaXIiLCJhIjoiY2toMmhrd2ZqMGVudjJzbXhvZDIzbmFqdSJ9.biQkpZ8loVXIsxhMuNffog"
-        onViewportChange={(viewport) => this.setState({ viewport })}
-        {...this.state.viewport}
-        className="map" >
-        <Marker latitude={53.475433} longitude={-2.250497} offsetLeft={-20} offsetTop={-10}>
-        <div className="marker-box">
-              <div className="marker-name">
-                  <i className="fas fa-signature"></i>&nbsp;
-                  <p className="marker-text">: Robert Moscaliuc</p>
-              </div>
-              <div className="marker-name">
-              <i className="far fa-envelope"></i>&nbsp;
-              
-                  <p className="marker-text">: robertmoscaliuc96gmail.com</p>
-              </div>
-              <div className="marker-name">
-                  <i className="fas fa-phone-alt"></i>
-                  <p>&nbsp;: 07770083427</p>
-              </div>
-             
-               
-   
-        
-        </div>
-        </Marker>
-          
-      </ReactMapGL>
+      <div className="map">
+        <ReactMapGL
+          mapStyle="mapbox://styles/mapbox/dark-v10"
+          mapboxApiAccessToken="pk.eyJ1IjoibW9zY2FsaXIiLCJhIjoiY2toMmhrd2ZqMGVudjJzbXhvZDIzbmFqdSJ9.biQkpZ8loVXIsxhMuNffog"
+          onViewportChange={(viewport) => this.setState({ viewport })}
+          {...this.state.viewport}
+          className="map" >
+          <Marker latitude={53.471433} longitude={-2.250497} offsetLeft={-20} offsetTop={-10}>
+          <div className="marker-box">
+                <div className="marker-name">
+                    <i className="fas fa-signature"></i>&nbsp;
+                    <p className="marker-text">: Robert Moscaliuc</p>
+                </div>
+                <div className="marker-name">
+                <i className="far fa-envelope"></i>&nbsp;
+                
+                    <p className="marker-text">: robertmoscaliuc96gmail.com</p>
+                </div>
+                <div className="marker-name">
+                    <i className="fas fa-phone-alt"></i>
+                    <p>&nbsp;: 07770083427</p>
+                </div>
+          </div>
+          </Marker>
+            
+        </ReactMapGL>
+
+      </div>
     );
   }
 }
--- pages/about.js
+++ pages/about.js
@@ -8,16 +8,25 @@ const About = () =>(
         <HeadInner/>
         <Navbar/>
             <div className="about-image">
-            <div className="about-inner">
-
-              <div className="about-text">
-                        <h1 className="large-about">Contact me</h1>
-                        <p className="paragraph">I am interested in any entry-level opportunities.</p>
+              <div className="about-inner">
+                <div className="about-text">
+                    <h1 className="large-about">Contact me</h1>
+                    <p className="paragraph-about">I am interested in any entry-level opportunities.</p>
+                <div className="contact-form">
+                    <form className="form">
+                      <div className="form-group">
+                        <input type="text" placeholder="Name"/>
+                        <input type="text" placeholder="Email"/>
+                      </div>
+                      <input type="text" placeholder="Subject"/>
+                      <textarea type="text" placeholder="Message"/>
+                      <button className="contact-button">Send</button>
+                          </form>
+                        </div>
                     </div>
-            
+                <div className="map-inner">
+                  <Map/>
 
-              <div className="map-inner">
-                 <Map/>
               </div>
             </div>
             </div>
